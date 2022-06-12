c463264c599f75918b1f9d56a4622d6372914d2f
bodascaluca <bodasca.luca@gmail.com>
2022-06-12 23:00:27 +0200

    Add prova

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include("./vue-dischi/database.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <title>php-ajax-dischi</title>
 </head>
 <body>

9091847b209661130975822a5f0bd71c24d32616
bodascaluca <bodasca.luca@gmail.com>
2022-06-13 23:53:43 +0200

    dovrebbe funzionare

--- index.json.php
+++ index.json.php
@@ -15,9 +15,11 @@
 <body>
     
     <main id="root"> 
-        <div v-for="data in database">
+        <div class="container" v-for="data in database">
+            <img :src="data.poster" />
             <h2>{{ data.title }}</h2>
             <h3>{{ data.author }}</h3>
+            <strong>{{ data.year }}</strong>
         </div>
     </main>
 
